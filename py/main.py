dcfbecab09a7224ac2b874d9bd6feb64d4ef3b1f
shamil200300 <98822830+shamil200300@users.noreply.github.com>
2022-03-03 14:01:44 +0300

    Add files via upload

---
+++ py/main.py
@@ -0,0 +1,20 @@
+from pickle import TRUE
+from random import *
+boss = randint(5,5)
+a1 = int(input()) 
+if a1 == 5 :
+    print("u right")
+else:
+    print("false")
+boss2 = randint(7,7)
+a2 = int(input()) 
+if a2 == 5 :
+    print("u right")
+else:
+    print("false")
+boss3 = randint(9,9)
+a3 = int(input()) 
+if a3 == 9 :
+    print("u right")
+else:
+    print("false")
